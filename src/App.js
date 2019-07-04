8ef79a1f6f6da3697db1992e5024956bfceec0a2
Alexander Kohonovsky <saniaky@gmail.com>
2019-07-03 21:53:54 -0500

    Add bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1>Planner</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-4">
+                    To Do
+                </div>
+                <div className="col-md-4">
+                    In Progress
+                </div>
+                <div className="col-md-4">
+                    Done
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
