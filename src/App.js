310c3a7105e65a982d9ad0655d9d1a2616c4b9e8
Alexander Kohonovsky <saniaky@gmail.com>
2019-07-30 21:02:55 -0500

    Add ui-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import Panel from "./panel/Panel";
+import {Route, BrowserRouter as Router} from "react-router-dom";
+import Main from "./main/MainPage";
+import Login from "./auth/LoginPage";
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import Panel from "./panel/Panel";
 
 function App() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1>Planner</h1>
+        <Router>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>Planner</h1>
+                    </div>
                 </div>
-            </div>
 
-            <div className="row">
-                <Panel title="To Do"/>
-                <Panel title="In Progress"/>
-                <Panel title="Done"/>
+                <div className="row">
+                    <Panel title="To Do"/>
+                    <Panel title="In Progress"/>
+                    <Panel title="Done"/>
+                </div>
+
+                <Route path="/" exact component={Main}/>
+                <Route path="/login/" component={Login}/>
             </div>
-        </div>
+        </Router>
     );
 }
 
